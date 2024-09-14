48a37f05a79423177dc8b245c9b52b25916fef16
Luis Di Nicco <102923308+LuisDiNicco@users.noreply.github.com>
2024-09-14 18:51:09 -0300

    Update contadorDeCaracteres.py

--- Trabajo Practico 1/Hilos/Python/contadorDeCaracteres.py
+++ Trabajo Practico 1/Hilos/Python/contadorDeCaracteres.py
@@ -8,82 +8,81 @@ ERROR_PARAMETROS = -1
 ERROR_ARCHIVO_INVALIDO = -2
 ERROR_HILOS = -3
 
-listaSubtotal = []
+lista_subtotal = []
 
-def validarParametros():
-    parser = argparse.ArgumentParser(description="Contador de caracteres dado un archivo usando múltiples hilos.")
-    parser.add_argument("rutaArchivo", type=str, help="Ruta del archivo.")
-    parser.add_argument("cantidadDeHilos", type=int, help="Cantidad de hilos para procesar el archivo.")
+def validar_parametros():
+  parser = argparse.ArgumentParser(description="Contador de caracteres dado un archivo usando múltiples hilos.")
+  parser.add_argument("rutaArchivo", type=str, help="Ruta del archivo.")
+  parser.add_argument("cantidadDeHilos", type=int, help="Cantidad de hilos para procesar el archivo.")
     
-    args = parser.parse_args()
+  args = parser.parse_args()
     
-    if not os.path.isfile(args.rutaArchivo):
-        print(f"Error: El archivo '{args.rutaArchivo}' no existe o la ruta es inválida.")
-        return None, None, ERROR_ARCHIVO_INVALIDO
+  if not os.path.isfile(args.rutaArchivo):
+    print(f"Error: El archivo '{args.rutaArchivo}' no existe o la ruta es inválida.")
+    return None, None, ERROR_ARCHIVO_INVALIDO
 
-    if args.cantidadDeHilos <= 0:
-        print("Error: La cantidad de hilos debe ser un número positivo.")
-        return None, None, ERROR_PARAMETROS
+  if args.cantidadDeHilos <= 0:
+    print("Error: La cantidad de hilos debe ser un número positivo.")
+    return None, None, ERROR_PARAMETROS
     
-    return args.rutaArchivo, args.cantidadDeHilos, VALIDACION_PARAMETROS_OK
+  return args.rutaArchivo, args.cantidadDeHilos, VALIDACION_PARAMETROS_OK
 
-def eliminarLineasVacias(archivo, vectorLineas):
-    with open(archivo, 'r') as f:
-        for linea in f:
-            if linea:
-                vectorLineas.append(linea.strip()) # Elimina saltos de linea
-                #vectorLineas.append(linea)
+def eliminar_lineas_vacias(archivo, vector_lineas):
+  with open(archivo, 'r') as f:
+    for linea in f:
+      if linea:
+        vector_lineas.append(linea.strip()) # Elimina saltos de linea
 
 def contarCaracteresEnRango(vectorLineas, indiceSuperior, indiceInferior):
-    subtotal = 0
-    for i in range(indiceSuperior, indiceInferior + 1):
-        subtotal += len(vectorLineas[i])
-    listaSubtotal.append(subtotal)
-    print(f"Resultado Parcial: {subtotal}")
+  subtotal = 0
+  for i in range(indiceSuperior, indiceInferior + 1):
+    subtotal += len(vectorLineas[i])
+  lista_subtotal.append(subtotal)
+  print(f"Resultado Parcial: {subtotal}")
 
-def procesarArchivo(vectorLineas, cantidadDeHilos):
-    cantidadDeLineasPorHilo = len(vectorLineas) // cantidadDeHilos
-    threads = []
+def procesar_archivo(vector_lineas, cantidad_de_hilos):
+  cantidad_de_lineas_por_hilo = len(vector_lineas) // cantidad_de_hilos
+  threads = []
     
-    for i in range(cantidadDeHilos):
-        indiceInferior = i * cantidadDeLineasPorHilo
-        indiceSuperior = ((i + 1) * cantidadDeLineasPorHilo) - 1
-        if i == cantidadDeHilos - 1 and len(vectorLineas) % cantidadDeHilos:
-            indiceSuperior += len(vectorLineas) % cantidadDeHilos
-        thread = threading.Thread(target=contarCaracteresEnRango, args=(vectorLineas, indiceInferior, indiceSuperior))
-        threads.append(thread)
-        thread.start()
+  for i in range(cantidad_de_hilos):
+    indice_inferior = i * cantidad_de_lineas_por_hilo
+    indice_superior = ((i + 1) * cantidad_de_lineas_por_hilo) - 1
+    if i == cantidad_de_hilos - 1 and len(vector_lineas) % cantidad_de_hilos:
+      indice_superior += len(vector_lineas) % cantidad_de_hilos
+    thread = threading.Thread(target=contarCaracteresEnRango, args=(vector_lineas, indice_inferior, indice_superior))
+    threads.append(thread)
+    thread.start()
     
-    for thread in threads:
-        thread.join()
+  for thread in threads:
+    thread.join()
 
 def main():
-    pid = os.getpid()
-    print(f"PID: {pid}")
+  pid = os.getpid()
+  print(f"PID: {pid}")
     
-    rutaArchivo, cantidadDeHilos, resultado = validarParametros()
+  ruta_archivo, cantidad_de_hilos, resultado = validar_parametros()
     
-    if resultado != VALIDACION_PARAMETROS_OK:
-        return resultado
+  if resultado != VALIDACION_PARAMETROS_OK:
+    return resultado
     
-    vectorLineas = []
-    eliminarLineasVacias(rutaArchivo, vectorLineas)
+  vector_lineas = []
+  eliminar_lineas_vacias(ruta_archivo, vector_lineas)
     
-    if len(vectorLineas) < cantidadDeHilos:
-        print("La cantidad de hilos supera la cantidad de lineas no vacias del archivo")
-        return ERROR_HILOS
+  if len(vector_lineas) < cantidad_de_hilos:
+    print("La cantidad de hilos supera la cantidad de lineas no vacias del archivo")
+    return ERROR_HILOS
     
-    inicio = time.time()
+  inicio = time.time()
     
-    procesarArchivo(vectorLineas, cantidadDeHilos)
+  procesar_archivo(vector_lineas, cantidad_de_hilos)
 
-    fin = time.time()
+  fin = time.time()
 
-    duracion_milisegundos = (fin - inicio) * 1000 
+  duracion_milisegundos = (fin - inicio) * 1000 
     
-    print(f"Tiempo de ejecución: {duracion_milisegundos:.2f} ms")
+  print(f"Tiempo de ejecución: {duracion_milisegundos:.2f} ms")
 
-    print(f"Resultado Total: {sum(listaSubtotal)}")
+  print(f"Resultado Total: {sum(lista_subtotal)}")
 
 if __name__ == "__main__":
-    main()
+  main()
