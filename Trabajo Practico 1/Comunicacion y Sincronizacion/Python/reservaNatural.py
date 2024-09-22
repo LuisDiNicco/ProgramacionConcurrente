1360e8e008f7d4ab0e1b6f572097bcd66148c945
Ivan <ioa2001@hotmail.com>
2024-09-22 11:50:09 -0300

    Agrego Ejercicio Comunicacion Python

--- Trabajo Practico 1/Comunicacion y Sincronizacion/Python/reservaNatural.py
+++ Trabajo Practico 1/Comunicacion y Sincronizacion/Python/reservaNatural.py
@@ -1,76 +1,79 @@
-from multiprocessing import Pipe
-import os
 import sys
 import threading
+import time
 
-proceso_hijo = 0
 cantidad_maxima_soga = 5
 
-mutex_direccion = threading.Lock()
-mutex_region_critica= threading.Lock()
-mutex_imprimir_mensaje = threading.Lock() 
-semaforo_soga = threading.Semaphore(cantidad_maxima_soga)
-cantidad_monos_lado_izq
-cantidad_monos_lado_der
-contador_monos_viajaron_lado_der = 0
+mutex_cuerda = threading.Lock()
+mutex_region_critica = threading.Lock()
 
-def comenzar_viaje_hacia_izquierda():
-  mutex_direccion.acquire()
-  if(cantidad_monos_lado_izq > 0):
-    mutex_region_critica.acquire()
-    cantidad_maxima_soga--
-    cantidad_monos_lado_der--
-    viajar_hacia_lado_izq():
-    #contador_monos_viajaron_lado_der++
-    mutex_region_critica.release()
+cantidad_monos = 0
+lado_cuerda = 0
+dueño = None  
 
-def comenzar_viaje_hacia_derecha():
-  mutex_direccion.acquire()
-  if(cantidad_monos_lado_der > 0):
-     mutex_region_critica.adquire()
-  mutex_region_critica.acquire()
-  print("Recibido: ", recepcion.recv())
-  contadorLadoB++
-  recepcion.close()
-  os._exit(0)
-  mutex_direccion.acquire()
-  contadorLadoA--
-  envio.send()
-  envio.close()
-  os.wait()
+def cambiar_de_lado(direccion):
+    global lado_cuerda, cantidad_monos, dueño
 
-def viajar_hacia_lado_der():
-   print("- - ->")
-   print("Viajando hacia el Lado Derecho...")
+    while True:
+      if direccion == lado_cuerda:
+        #mutex_region_critica.acquire()
+        if cantidad_monos < cantidad_maxima_soga:
+          #mutex_region_critica.release()
+          with mutex_region_critica:  
+            cantidad_monos += 1
+            print(f"Viajando hacia la {'derecha' if 1+direccion%2 == 1 else 'izquierda'}, "
+                  f"cantidad de monos en cuerda: {cantidad_monos}")
+            if dueño==threading.get_ident():
+              mutex_cuerda.release()
 
-def viajar_hacia_lado_izq():
-   print("<- - -")
-   print("Viajando hacia el Lado Izquierdo...")
+          time.sleep(2)
+
+          with mutex_region_critica:
+            cantidad_monos -= 1
+            print(f"Mono llegó a la {'derecha' if 1+direccion%2 == 1 else 'izquierda'}, "
+            f"cantidad de monos en cuerda: {cantidad_monos}")
+
+            if cantidad_monos == 0:
+                print("No hay más monos en la cuerda, se libera.")
+            return
+        else:
+          #mutex_region_critica.release()
+          continue
+      else:
+        mutex_region_critica.acquire()
+        if (cantidad_monos == 0):
+          lado_cuerda=direccion
+          mutex_cuerda.acquire()
+          dueño=threading.get_ident()
+          print("-------------------------------------------------------------------------------------")
+          print(f"Cambiando la dirección de la cuerda hacia la {'derecha' if direccion == 1 else 'izquierda'}")
+          mutex_region_critica.release()
+        else:
+          mutex_region_critica.release()
+
 
 def main():
-    cantidad_monos_lado_izq = sys.argv[1]
-    if(cantidad_monos_lado_izq < 0):
-      sys.exit("La cantidad de monos del lado izquierdo no puede ser negativa")  
+    if len(sys.argv) < 2:
+        sys.exit("Se debe indicar la cantidad de monos")
+        return
+    cantidad_monos_a_generar = int(sys.argv[1])
+    if cantidad_monos_a_generar < 0:
+        sys.exit("La cantidad de monos no puede ser negativa")
 
-    cantidad_monos_lado_der = sys.argv[2]
-    if(cantidad_monos_lado_der < 0):
-      sys.exit("La cantidad de monos del lado derecho no puede ser negativa")  
-    
-    threadsMonosIzquierda = []
+    threads = []
 
-    threadsMonosDerecha = []
+    lado = 1
+    for i in range(cantidad_monos_a_generar):
+        babuino = threading.Thread(target=cambiar_de_lado, args=(lado,))
+        threads.append(babuino)
+        lado = 1 + lado % 2  
 
-    for i in range(cantidad_monos_lado_izq):
-        threadMono = threading.Thread(target=comenzar_viaje_hacia_izquierda, name="Thread " + i)
-        threadsMonosIzquierda.append(threadMono)
-        threadMono.start()
+    for thread in threads:
+        thread.start()
 
-    for i in range(cantidad_monos_lado_der):
-        threadMono = threading.Thread(target=comenzar_viaje_hacia_derecha, name="Thread " + i)
-        threadsMonosDerecha.append(threadMono)
-        threadMono.start()
+    for thread in threads:
+        thread.join()
 
-    recepcion, envio = Pipe(True)
-    
+
 if __name__ == "__main__":
     main()
