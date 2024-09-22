99105451122e2c396baecd26e804a74572d28b82
leandro <leandrojuan1998@gmail.com>
2024-09-22 14:30:23 -0400

    Borrado de comentarios python procesos

--- Trabajo Practico 1/Procesos/Java/Proceso.java
+++ Trabajo Practico 1/Procesos/Java/Proceso.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import java.lang.management.ManagementFactory;
 
 public class Proceso
 {
--- Trabajo Practico 1/Procesos/Python/arbolDeProcesos.py
+++ Trabajo Practico 1/Procesos/Python/arbolDeProcesos.py
@@ -2,12 +2,10 @@ import os
 import time
 import sys
 
-# Constantes Descriptivas
 ERROR_CREACION_PROCESO = 2
 TIEMPO_DE_ESPERA_PARA_MOSTRAR_ARBOL = 10
 
 def crear_proceso(nombre):
-  """Crea un proceso hijo con el nombre especificado."""
   pid = os.fork()
   if pid < 0:
     return ERROR_CREACION_PROCESO
@@ -17,67 +15,62 @@ def crear_proceso(nombre):
   return pid
 
 def proceso_b():
-  """Crea los procesos D y C."""
   pid_d = crear_proceso("D")
   if pid_d == 0:
     proceso_d()
-  os.wait()           # Wait de B
-  os._exit(0)         # Exit de B
+  os.wait()
+  os._exit(0)
 
 def proceso_c():
-  """Crea los procesos E y C."""
   pid_e = crear_proceso("E")
   if pid_e == 0:
     proceso_e()
-  os.wait()           # Wait de C
-  os._exit(0)         # Exit de C
+  os.wait()
+  os._exit(0)
 
 def proceso_d():
-  """Crea los procesos F y G."""
   pid_f = crear_proceso("F")
   if pid_f != 0:
     pid_g = crear_proceso("G")
     if pid_g != 0:
-      os.wait()       # Wait de D
       os.wait()
-      os._exit(0)     # Exit de D
+      os.wait()
+      os._exit(0)
     else:
-      proceso_hoja()  # Exit de G
+      proceso_hoja()
   else:
-    proceso_hoja()    # Exit de F 
+    proceso_hoja()
 
 def proceso_e():
-  """Crea los procesos H e I."""
   pid_h = crear_proceso("H")
   if pid_h != 0:
     pid_i = crear_proceso("I")
     if pid_i != 0:
-      os.wait()       # Wait de E
       os.wait()
-      os._exit(0)     # Exit de E
+      os.wait()
+      os._exit(0)
     else:
-      proceso_hoja()  # Exit de I
+      proceso_hoja()
   else:
-    proceso_hoja()    # Exit de H
+    proceso_hoja()
 
 def proceso_hoja():
   time.sleep(TIEMPO_DE_ESPERA_PARA_MOSTRAR_ARBOL)
-  os._exit(0) 
+  os._exit(0)
 
 def crear_jerarquia_de_procesos():
-  """Crea una jerarquía de procesos (A, B, C, D, E, F, G, H, I)."""
   pid_b = crear_proceso("B")
-  if pid_b != 0:      # Estoy en A
+  if pid_b != 0:
     print("Proceso A:", os.getpid())
     sys.stdout.flush()
-    os.wait()         # Wait de A
-    os._exit(0)       # Exit de A
-  else:               # Estoy en B
+    os.wait()
+    os._exit(0)
+  else:
     pid_c = crear_proceso("C")
     if pid_c != 0:
-      proceso_b()     # Estoy en B
+      proceso_b()
     else:
-      proceso_c()     # Estoy en C
+      proceso_c()
 
 if __name__ == "__main__":
   crear_jerarquia_de_procesos()
