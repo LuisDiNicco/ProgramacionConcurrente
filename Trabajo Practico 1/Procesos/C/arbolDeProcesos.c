ad5f17017185c145f12d56509ffb2c08908e0953
leandro <leandrojuan1998@gmail.com>
2024-09-22 12:31:24 -0400

    Agrego ejercicio de C de procesos con correcciones

--- Trabajo Practico 1/Procesos/C/arbolDeProcesos.c
+++ Trabajo Practico 1/Procesos/C/arbolDeProcesos.c
@@ -4,14 +4,16 @@
 #include <sys/wait.h>
 
 #define ERROR_CREACION_PROCESO 2
-#define TIEMPO_DE_ESPERA_PARA_CREACION 10
-#define TIEMPO_ESPERA_FINAL 10
+#define TIEMPO_DE_ESPERA_PARA_MOSTRAR_ARBOL 10
 
 pid_t crearProceso(char nombre);
 void manejarProcesoA();
 void manejarProcesoB();
 void manejarProcesoC();
 void manejarProcesoD();
+void manejarProcesoE();
+void manejarProcesoHoja();
+int main();
 
 pid_t crearProceso(char nombre)
 {
@@ -25,7 +27,6 @@ pid_t crearProceso(char nombre)
     printf("Proceso %c: %d\n", nombre, getpid());
     fflush(stdout);
   }
-  sleep(TIEMPO_DE_ESPERA_PARA_CREACION);
   return pid;
 }
 
@@ -60,6 +61,20 @@ void manejarProcesoB()
   }
 }
 
+void manejarProcesoC()
+{
+  pid_t pid_e = crearProceso('E');
+  if (pid_e == 0)  // Estoy en E
+  {
+    manejarProcesoE();
+  }
+  else
+  {
+    wait(NULL);      // Wait de C
+    exit(0);
+  }
+}
+
 void manejarProcesoD()
 {
   pid_t pid_d = crearProceso('D');
@@ -76,51 +91,47 @@ void manejarProcesoD()
       }
       else
       {
-        exit(0);     // Exit de G
+        manejarProcesoHoja(); // Exit de G   
       }
     }
     else
     {
-      exit(0);       // Exit de F
+        manejarProcesoHoja(); // Exit de F
     }
   }
 }
 
-void manejarProcesoC()
+void manejarProcesoE()
 {
-  pid_t pid_e = crearProceso('E');
-  if (pid_e == 0)  // Estoy en E
+  pid_t pid_h = crearProceso('H');
+  if (pid_h != 0)
   {
-    pid_t pid_h = crearProceso('H');
-    if (pid_h != 0)
+    pid_t pid_i = crearProceso('I');
+    if (pid_i != 0)
     {
-      pid_t pid_i = crearProceso('I');
-      if (pid_i != 0)
-      {
-        wait(NULL);  // Wait de E
-        wait(NULL);
-        exit(0);     // Exit de E
-      }
-      else
-      {
-        exit(0);     // Exit de I
-      }
+      wait(NULL);  // Wait de E
+      wait(NULL);
+      exit(0);     // Exit de E
     }
     else
     {
-      exit(0);       // Exit de H
+      manejarProcesoHoja(); // Exit de I
     }
   }
   else
   {
-    wait(NULL);      // Wait de C
-    exit(0);
+    manejarProcesoHoja(); // Exit de H      
   }
+}
+
+void manejarProcesoHoja()
+{
+  sleep(TIEMPO_DE_ESPERA_PARA_MOSTRAR_ARBOL);
+  exit(0);
 }
 
 int main()
 {
   manejarProcesoA();
-  sleep(TIEMPO_ESPERA_FINAL);
   return 0;
 }
